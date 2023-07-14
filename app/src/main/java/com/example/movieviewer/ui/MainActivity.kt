949accae0f6813f90222e5b646d11354f01ab714
hubinsord <hubinsord@gmail.com>
2023-07-14 11:45:45 +0200

    updated java version, fixed compiling issues

--- app/src/main/java/com/example/movieviewer/ui/MainActivity.kt
+++ app/src/main/java/com/example/movieviewer/ui/MainActivity.kt
@@ -2,15 +2,8 @@ package com.example.movieviewer.ui
 
 import android.os.Bundle
 import android.os.PersistableBundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.movieviewer.R
-import com.example.movieviewer.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
--- app/src/main/java/com/example/movieviewer/ui/favoritelist/FavoritesListViewModel.kt
+++ app/src/main/java/com/example/movieviewer/ui/favoritelist/FavoritesListViewModel.kt
@@ -2,7 +2,8 @@ package com.example.movieviewer.ui.favoritelist
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class FavoritesListViewModel: ViewModel() {
+class FavoritesListViewModel @Inject constructor() : ViewModel() {
 }
